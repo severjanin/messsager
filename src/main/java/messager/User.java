519f778a1810999c860bfbd66a6a778903762033
severjanin <severjanin@gmail.com>
2018-08-29 19:32:59 +0300

    Initial commit

---
+++ src/main/java/messager/User.java
@@ -0,0 +1,13 @@
+package messager;
+
+import java.time.Clock;
+
+public class User {
+    private String email;
+    private String name;
+    private int age;
+    private String password;
+    private Clock lastLoginTime;
+    
+
+}
